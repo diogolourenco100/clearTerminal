d808a8c7dd454b2891ba33abb20b875459a7e6e1
Diogo S. Lourenço <114771411+diogolourenco100@users.noreply.github.com>
2024-05-26 20:51:51 -0300

    Update clearTerminal.py

--- clearTerminal.py
+++ clearTerminal.py
@@ -2,9 +2,18 @@ import os
 import platform
 
 def clear():
-    operational_system = platform.system()
-
-    if operational_system == 'Windows':
-        os.system('cls')
-    else:
-        os.system('clear')
+    os_name = platform.system()
+    try:
+        if os_name == "Windows":
+            os.system('cls')
+        else:
+            os.system('clear')
+    except Exception as e:
+        pass
+        try:
+            if os_name == "Windows":
+                os.system('clear')
+            else:
+                os.system('cls')
+        except Exception as e:
+            print(f'ERROR: {e}')
